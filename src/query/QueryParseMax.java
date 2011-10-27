c9fe61340eeb5936108c622c95ec6abddd134cbe
contact@thomasgoossens.be <tgoossens@users.noreply.github.com>
2011-10-27 14:26:36 +0000

    bug fix max parse method

--- src/query/QueryParseMax.java
+++ src/query/QueryParseMax.java
@@ -16,7 +16,7 @@ public class QueryParseMax implements QueryParseMethod {
 	private long calculateMax(List<Activity> activities,QueryParseField parseField){
 			long max = parseField.getValue(activities.get(0));
 			for(int i=0;i<activities.size();i++){
-				max += Math.max(max,parseField.getValue(activities.get(i)));
+				max = Math.max(max,parseField.getValue(activities.get(i)));
 			}
 	return max;
 	}
