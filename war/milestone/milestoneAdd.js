efbe0affb5acf783a752fedfb9eb73abc0896850
a.buttiens@gmail.com <a.buttiens@gmail.com@e6e189a0-43b2-ad37-5ef3-fa62078521ab>
2011-12-07 15:01:21 +0000

    Fixed the string representation of the dates

--- war/milestone/milestoneAdd.js
+++ war/milestone/milestoneAdd.js
@@ -74,11 +74,25 @@
 		var startDate = $("#start-date").data("datebox").theDate,
 			stopDate = $("#stop-date").data("datebox").theDate;
 		
+		var month=new Array(12);
+		month[0]="January";
+		month[1]="February";
+		month[2]="March";
+		month[3]="April";
+		month[4]="May";
+		month[5]="June";
+		month[6]="July";
+		month[7]="August";
+		month[8]="September";
+		month[9]="October";
+		month[10]="November";
+		month[11]="December";
+		
 		var sentence = "";
 		sentence += "between "
-		sentence += startDate.toDateString();
+		sentence += startDate.getDate() + " " + month[startDate.getMonth()];
 		sentence += " and "
-		sentence += stopDate.toDateString();
+		sentence += stopDate.getDate() + " " + month[stopDate.getMonth()];
 		
 		return sentence;
 	}
